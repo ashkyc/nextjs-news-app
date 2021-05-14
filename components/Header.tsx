57d01ff748c2e7a93ad86793d2619e1e46336c2f
Ashley Man <aman@sold.com>
2021-05-14 00:14:03 -0700

    cadded news apikey in config file and added file to gitignore

---
+++ components/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from '../styles/Header.module.scss';
+
+export const Header = () => {
+  return <div></div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
+import fetch from 'node-fetch';
+import { NEXT_PUBLIC_NEWS_KEY } from '../config';
 
-export default function Home() {
+export default function Home({ newsFeed }) {
+  console.log(newsFeed);
   return (
     <div>
       <h1>Welcome To Next.js News App</h1>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  // const res = await fetch(
+  //   `https://jsonplaceholder.typicode.com/posts?_limit=6`,
+  // );
+  // const articles = await res.json();
+
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${NEXT_PUBLIC_NEWS_KEY}`,
+  );
+  const newsFeed = await res.json();
+
+  return {
+    props: {
+      newsFeed,
+    },
+  };
+};
